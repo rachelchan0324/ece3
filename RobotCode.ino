95f37d93ae4889ca6b020b2d9de9e401bd6a0657
Rachel Chan <rachelytchan0324@gmail.com>
2024-11-24 18:47:09 -0800

    Add files via upload

--- RobotCode.ino
+++ RobotCode.ino
@@ -2,13 +2,8 @@
 
 // SENSOR VARIABLES
 uint16_t sensorValues[8];
-// uint16_t sensorMinimums[8] = {748, 652, 627, 627, 652, 604, 627, 676}; // sophia's bot
-// uint16_t sensorMinimums[8]= {435, 389, 435, 435, 412, 458, 480, 642}; // rachel's bot (OG)
-int sensorMinimums[8] = {534, 558, 606, 582, 511, 558, 606, 630}; // rachel's bot - recalibrated
-// int sensorMinimums[8] = {771, 723, 675, 651, 651, 627, 627, 675 }; // sophia's bot - recalibrated
-// uint16_t sensorMaximums[8] = {1611, 1872, 2465, 1920, 1398, 1943, 2180, 2299}; // rachel's bot (OG)
-int sensorMaximums[8] = {2142, 2500, 2500, 2326, 1981, 2465, 2500, 2500}; // rachel's bot - recalibrated
-// uint16_t sensorMaximums[8] = {2500, 2500, 2500, 2500, 2500}; // sophia's bot - recalibrated
+int sensorMinimums[8] = {534, 558, 606, 582, 511, 558, 606, 630};
+int sensorMaximums[8] = {2142, 2500, 2500, 2326, 1981, 2465, 2500, 2500};
 int sensorWeights[8] = {-15, -14, -12, -8, 8, 12, 14, 15};
 
 // PIN VARIABLES
@@ -24,8 +19,12 @@ const int right_pwm_pin = 39;
 float kp = 0.04;
 float kd = 0.05;
 float prev_error;
-int base_speed = 37;
-bool first_run = true;
+const int base_speed = 37;
+bool loop_done = false;
+bool arch_done = false;
+bool harpin_done = false;
+bool turnaround_done = false;
+int encoder_count = 0;
 
 void setup() {
   ECE3_Init();
@@ -44,52 +43,26 @@ void setup() {
   digitalWrite(right_dir_pin, LOW);
   digitalWrite(right_nslp_pin, HIGH);
   pinMode(LED_RF, OUTPUT);
+
+  resetEncoderCount_left();
+  resetEncoderCount_right();
 }
 
 void loop() {
-  ECE3_read_IR(sensorValues);
-  
-  float error = 0;
-  float normalizedValue;
+  float error = calculateError();
 
-  // calculate error
-  for (int i = 0; i < 8; i++) {
-    // normalization of sensorValues
-    if (sensorValues[i] < sensorMinimums[i])
-      sensorMinimums[i] = sensorValues[i];
-    if (sensorValues[i] > sensorMaximums[i])
-      sensorMaximums[i] = sensorValues[i];
-
-    normalizedValue = sensorValues[i];
-    normalizedValue -= sensorMinimums[i];
-    normalizedValue *= 1000;
-    normalizedValue /= (sensorMaximums[i] - sensorMinimums[i]);
-    error += (normalizedValue * sensorWeights[i]);
-  }
-  error /= 24;
- 
-  if (error == 0) // 
+  if(error == 0 && !loop_done) // loop
   {
-      int left_encoder_count = 0;
-      int right_encoder_count = 0;
-      resetEncoderCount_left();
-      resetEncoderCount_right();
-
-      while(abs(left_encoder_count) < 375)
-      {
-        analogWrite(left_pwm_pin, base_speed);
-        analogWrite(right_pwm_pin, base_speed);
-        digitalWrite(left_dir_pin, HIGH); // one motor forward
-        digitalWrite(right_dir_pin, LOW); // other motor backward
-        left_encoder_count = getEncoderCount_left();
-        right_encoder_count = getEncoderCount_right();
-      }
-  
-      digitalWrite(left_dir_pin, LOW); // back to forward
-      return;
+    loopTurn();
+    return;
+  }
+  elif (error == 0 && loop_done && !turnaround_done) // turnaround 
+  {
+    turnaround();
+    return; // needs to get new sensorValues
   }
   
-  if(first_run)
+  if(first_run) // relies only on kp
   {
       float proportional_term = error * kp;
       analogWrite(left_pwm_pin, base_speed - proportional_term);
@@ -104,7 +77,73 @@ void loop() {
     analogWrite(left_pwm_pin, base_speed - proportional_term - derivative_term);
     analogWrite(right_pwm_pin, base_speed + proportional_term + derivative_term);
   }
-
-  prev_error = error;
+  encoder_count = int((abs(getEncoderCount_left()) + abs(getEncoderCount_right())) / 2.0); // update encoder counts
+  prev_error = error; 
   first_run = false;
+}
+
+float calculateError()
+{
+  float error = 0;
+  ECE3_read_IR(sensorValues);
+  float normalizedValue;
+  for (int i = 0; i < 8; i++) {
+    // normalization of sensorValues
+    if (sensorValues[i] < sensorMinimums[i])
+      sensorMinimums[i] = sensorValues[i];
+    if (sensorValues[i] > sensorMaximums[i])
+      sensorMaximums[i] = sensorValues[i];
+
+    normalizedValue = sensorValues[i];
+    normalizedValue -= sensorMinimums[i];
+    normalizedValue *= 1000;
+    normalizedValue /= (sensorMaximums[i] - sensorMinimums[i]);
+    error += (normalizedValue * sensorWeights[i]);
+  }
+  error /= 24;
+  return error;
+}
+
+void turnaround()
+{
+  int starting_encoder_count = encoder_count;
+  while(abs(encoder_count - starting_encoder_count) < 375) // 375 encoder counts for 180 turn
+  {
+    analogWrite(left_pwm_pin, base_speed);
+    analogWrite(right_pwm_pin, base_speed);
+    digitalWrite(left_dir_pin, HIGH); // one motor forward
+    digitalWrite(right_dir_pin, LOW); // other motor backward
+    encoder_count = int((abs(getEncoderCount_left()) + abs(getEncoderCount_right())) / 2.0);
+  }
+  digitalWrite(left_dir_pin, LOW); // back to forward
+  turnaround_done = true;
+}
+
+void loopTurn()
+{
+  int starting_encoder_count = encoder_count;
+  // move forward past the horizontal line
+  while(abs(encoder_count - starting_encoder_count) < 125)
+  {
+    analogWrite(left_pwm_pin, base_speed);
+    analogWrite(right_pwm_pin, base_speed);
+    encoder_count = int((abs(getEncoderCount_left()) + abs(getEncoderCount_right())) / 2.0); // update encoder counts
+  }
+
+  starting_encoder_count = encoder_count;
+  // perform hardcoded loop
+  while(abs(encoder_count - starting_encoder_count) < 550)
+  {
+    analogWrite(left_pwm_pin, 125);
+    analogWrite(right_pwm_pin, 0.1);
+    delay(100);
+
+    encoder_count = int((abs(getEncoderCount_left()) + abs(getEncoderCount_right())) / 2.0); // update encoder counts
+
+    analogWrite(left_pwm_pin, 0);
+    analogWrite(right_pwm_pin, 0);
+    delay(100);  // Wait 1 second - adjust time as needed
+  }
+  prev_error = calculateError();
+  loop_done = true;
 }
