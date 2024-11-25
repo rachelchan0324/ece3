57fcf8528c2561b0ade685a215bff30e0d16bf8d
Rachel Chan <rachelytchan0324@gmail.com>
2024-11-24 17:08:40 -0800

    Add files via upload

--- RobotCode.ino
+++ RobotCode.ino
@@ -4,12 +4,12 @@
 uint16_t sensorValues[8];
 // uint16_t sensorMinimums[8] = {748, 652, 627, 627, 652, 604, 627, 676}; // sophia's bot
 // uint16_t sensorMinimums[8]= {435, 389, 435, 435, 412, 458, 480, 642}; // rachel's bot (OG)
-uint16_t sensorMinimums[8] = {534, 558, 606, 582, 511, 558, 606, 630}; // rachel's bot - recalibrated
+int sensorMinimums[8] = {534, 558, 606, 582, 511, 558, 606, 630}; // rachel's bot - recalibrated
 // int sensorMinimums[8] = {771, 723, 675, 651, 651, 627, 627, 675 }; // sophia's bot - recalibrated
 // uint16_t sensorMaximums[8] = {1611, 1872, 2465, 1920, 1398, 1943, 2180, 2299}; // rachel's bot (OG)
 int sensorMaximums[8] = {2142, 2500, 2500, 2326, 1981, 2465, 2500, 2500}; // rachel's bot - recalibrated
 // uint16_t sensorMaximums[8] = {2500, 2500, 2500, 2500, 2500}; // sophia's bot - recalibrated
-int32_t sensorWeights[8] = {-8, -4, -2, -1, 1, 2, 4, 8};
+int sensorWeights[8] = {-15, -14, -12, -8, 8, 12, 14, 15};
 
 // PIN VARIABLES
 const int left_nslp_pin=31; // nslp ==> awake & ready for PWM
@@ -21,12 +21,11 @@ const int right_nslp_pin = 11;
 const int right_pwm_pin = 39;
 
 // CODE VARIABLES
-float kp = 0.05;
-float kd = 0.035;
-int32_t prev_error;
-int32_t base_speed = 30;
+float kp = 0.04;
+float kd = 0.05;
+float prev_error;
+int base_speed = 37;
 bool first_run = true;
-int32_t horiz_black_line = 0; 
 
 void setup() {
   ECE3_Init();
@@ -38,7 +37,7 @@ void setup() {
   pinMode(left_dir_pin,OUTPUT);
   pinMode(left_pwm_pin,OUTPUT);
   digitalWrite(left_dir_pin,LOW);
-  digitalWrite(left_nslp_pin,HIGH);
+  digitalWrite(left_nslp_pin, HIGH);
   pinMode(right_nslp_pin, OUTPUT);
   pinMode(right_dir_pin, OUTPUT);
   pinMode(right_pwm_pin, OUTPUT);
@@ -50,21 +49,16 @@ void setup() {
 void loop() {
   ECE3_read_IR(sensorValues);
   
-  /**  
-  int32_t error = 0;
-  int normalizedValue;
-  bool isBlackLine = true;
+  float error = 0;
+  float normalizedValue;
 
   // calculate error
-  for (unsigned char i = 0; i < 8; i++) {
+  for (int i = 0; i < 8; i++) {
     // normalization of sensorValues
     if (sensorValues[i] < sensorMinimums[i])
       sensorMinimums[i] = sensorValues[i];
     if (sensorValues[i] > sensorMaximums[i])
       sensorMaximums[i] = sensorValues[i];
-    // phantom cross + turnaround check: within 20% of maximums
-    if (abs(sensorValues[i] - sensorMaximums[i]) > sensorMaximums[i] * 0.1)
-      isBlackLine = false;
 
     normalizedValue = sensorValues[i];
     normalizedValue -= sensorMinimums[i];
@@ -72,17 +66,10 @@ void loop() {
     normalizedValue /= (sensorMaximums[i] - sensorMinimums[i]);
     error += (normalizedValue * sensorWeights[i]);
   }
-  error /= 4;
-
-  if (isBlackLine)
-    horiz_black_line += 1;
-    Serial.print("Black Line ");
-    Serial.print(horiz_black_line);
-    Serial.print("\n");
-
-  if (horiz_black_line)
-  { **/
-  
+  error /= 24;
+ 
+  if (error == 0) // 
+  {
       int left_encoder_count = 0;
       int right_encoder_count = 0;
       resetEncoderCount_left();
@@ -97,26 +84,22 @@ void loop() {
         left_encoder_count = getEncoderCount_left();
         right_encoder_count = getEncoderCount_right();
       }
-
-      analogWrite(left_pwm_pin, 0);
-      analogWrite(right_pwm_pin, 0);
-      while(true)
-        delay(10000);
-
-      // digitalWrite(left_dir_pin, LOW); // back to forward
-  /** }
+  
+      digitalWrite(left_dir_pin, LOW); // back to forward
+      return;
+  }
   
   if(first_run)
   {
-      int32_t proportional_term = error * kp;
+      float proportional_term = error * kp;
       analogWrite(left_pwm_pin, base_speed - proportional_term);
       analogWrite(right_pwm_pin, base_speed + proportional_term);
   }
   else
   {
-    double proportional_term = error * kp;
-    double derivative = error - prev_error;
-    double derivative_term = derivative * kd;
+    float proportional_term = error * kp;
+    float derivative = error - prev_error;
+    float derivative_term = derivative * kd;
 
     analogWrite(left_pwm_pin, base_speed - proportional_term - derivative_term);
     analogWrite(right_pwm_pin, base_speed + proportional_term + derivative_term);
@@ -124,5 +107,4 @@ void loop() {
 
   prev_error = error;
   first_run = false;
-  **/
 }
